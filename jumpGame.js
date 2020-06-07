569f43c9f53c3bf3588311252f69e2168e54fe1c
wlyhdg <waelwc3@gmail.com>
2020-06-06 23:09:00 -0700

    Create jumpGame.js

---
+++ jumpGame.js
@@ -0,0 +1,10 @@
+/*
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+*/
+
+var canJump = function(nums) {
+    // inital value equals first index
+    // new value is index + index value
+};
